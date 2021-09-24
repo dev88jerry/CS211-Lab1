97e96a1ff0f9907d5c1d6f641d7264e6a2b70eb7
dev88jerry <70291890+dev88jerry@users.noreply.github.com>
2021-09-24 16:12:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 /**
-
+CS211 Lab 1 Exercise 1
 Jerry Lau
-CS211 Exercise 1
+002310931
 Oct 7 2021
+
+Program takes inputs from the keyboard by the user to output according calculations
 */
 
 #include <iostream>
@@ -11,15 +13,18 @@ Oct 7 2021
 using namespace std;
 
 int main(){
-    
+
+	///variables declaration
     string lName;
     int familySize;
     double income;
     double debts;
 	
-    const int EXP = 3000;
-    const double SAV = 0.02;
+	///const declaration for calculations
+	const int EXP = 3000;
+	const double SAV = 0.02;
     
+	///prompt user input
     cout << "What is your family name?" << endl;
     cin >> lName;
     
@@ -32,10 +37,12 @@ int main(){
     cout << "What is the family debt?" << endl;
 	cin >> debts;
     
-    int expenses = EXP * familySize;
+	///variables declaration and calculations
+	int expenses = EXP * familySize;
     double payments = debts / 12.0;
     double savings = familySize*(SAV*(income-debts));
     
+	///program output of calculations
 	cout << endl << "*******************************" << endl;
 	cout << "Family Budget Assistance Center" << endl;	
 	cout << "*******************************" << endl << endl;
@@ -47,7 +54,7 @@ int main(){
 	
 	cout << endl << "*******************************" << endl;
 	cout << "Predicted family living expenses: $" << expenses << endl;
-	cout << "The monthly payments necessary to pay off the debt in one year: $" << payments << endl;
+	cout << "The monthly payments necessary to pay off the debt in one year: $" << payments << endl;	
 	cout << "The amount of money your family should save: $" << savings << endl;
     
     return 0;
